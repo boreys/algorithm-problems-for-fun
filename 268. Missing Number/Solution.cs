e7a18937aa2468cae941ed5730a285f91c3d4c8e
Borey-mac <boreys@live.com>
2019-03-11 01:08:33 -0700

    Missing Number

--- 268. Missing Number/Solution.cs
+++ 268. Missing Number/Solution.cs
@@ -1,42 +1,5 @@
 public class Solution {
     //runtime: O(n), space complexity: O(1)
-    public int FirstMissingPositive(int[] nums) {
-        if(nums == null || nums.Length < 1){
-            return 1;
-        }
-        int n = nums.Length;
-        bool shouldKeepSwapping = false;
-        for(int i=0;i<n;i++){
-            shouldKeepSwapping = true;
-            while (shouldKeepSwapping && nums[i] > 0 && nums[i] < n)
-            {
-                int pos = nums[i] - 1;
-                if(pos != i && nums[i] != nums[pos])
-                {
-                    swap(nums, pos, i);
-                    shouldKeepSwapping = (nums[i] > 0 && nums[i] != pos);
-                }
-                else
-                {
-                    shouldKeepSwapping = false;
-                }
-            }
-        }
-        int missing = 1;
-        for(int i=0;i<nums.Length;i++){
-            if(nums[i] != missing){
-                return missing;
-            }
-            missing++;
-        }
-        return missing;
-    }
-    void swap(int[] A, int i, int j){
-        int tmp = A[i];
-        A[i] = A[j];
-        A[j] = tmp;
-    }
-    //runtime: O(n), space complexity: O(1)
     public int MissingNumber(int[] nums) {
         if(nums == null || nums.Length < 1){
             return 0;
