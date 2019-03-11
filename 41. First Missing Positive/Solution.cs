daf366d11723aa36601ca8646d02c1ec7f7a960b
Borey-mac <boreys@live.com>
2019-03-11 01:09:11 -0700

    First missing positive with runtime O(n)

--- 41. First Missing Positive/Solution.cs
+++ 41. First Missing Positive/Solution.cs
@@ -1,4 +1,41 @@
 public class Solution {
+    //runtime: O(n), space complexity: O(1)
+    public int FirstMissingPositive(int[] nums) {
+        if(nums == null || nums.Length < 1){
+            return 1;
+        }
+        int n = nums.Length;
+        bool shouldKeepSwapping = false;
+        for(int i=0;i<n;i++){
+            shouldKeepSwapping = true;
+            while (shouldKeepSwapping && nums[i] > 0 && nums[i] < n)
+            {
+                int pos = nums[i] - 1;
+                if(pos != i && nums[i] != nums[pos])
+                {
+                    swap(nums, pos, i);
+                    shouldKeepSwapping = (nums[i] > 0 && nums[i] != pos);
+                }
+                else
+                {
+                    shouldKeepSwapping = false;
+                }
+            }
+        }
+        int missing = 1;
+        for(int i=0;i<nums.Length;i++){
+            if(nums[i] != missing){
+                return missing;
+            }
+            missing++;
+        }
+        return missing;
+    }
+    void swap(int[] A, int i, int j){
+        int tmp = A[i];
+        A[i] = A[j];
+        A[j] = tmp;
+    }
     //runtime: O(n), space complexity: O(n)
     public int FirstMissingPositive(int[] nums) {
         if(nums == null || nums.Length < 1){
